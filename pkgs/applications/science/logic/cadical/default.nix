0c88500c709f44b6c9cc11d01e9cbfa5e2505a05
Narazaki, Shuji <shujinarazaki@protonmail.com>
2020-03-24 00:18:53 +0900

    update the expression
        * remove zlib from buildInputs
        * set maintainers
        * update description
    
    	modified:   pkgs/applications/science/logic/cadical/default.nix

--- pkgs/applications/science/logic/cadical/default.nix
+++ pkgs/applications/science/logic/cadical/default.nix
@@ -1,4 +1,4 @@
-{ stdenv, fetchFromGitHub, zlib }:
+{ stdenv, fetchFromGitHub }:
 
 stdenv.mkDerivation rec {
   pname = "cadical";
@@ -13,17 +13,18 @@ stdenv.mkDerivation rec {
 
   configurePhase = "./configure";
   # nativeBuildInputs = [ make ];
-  buildInputs = [ zlib ];
+  buildInputs = [ ];
   installPhase = ''
-    install -Dm0755 build/{cadical,mobical} $out/bin/cadical
+    install -Dm0755 build/cadical "$out/bin/cadical"
+    install -Dm0755 build/mobical "$out/bin/mobical"
     mkdir -p "$out/share/doc/${pname}-${version}/"
     install -Dm0755 {LICEN?E,README*,VERSION} "$out/share/doc/${pname}-${version}/"
   '';
 
 
   meta = with stdenv.lib; {
-    description = "CaDiCaL Simplified Satisfiability Solver";
-    # maintainers = with maintainers; [ gebner raskin ];
+    description = "Simplified Satisfiability Solver";
+    maintainers = with maintainers; [ shnarazk ];
     # platforms = platforms.unix;
     license = licenses.mit;
     homepage = http://fmv.jku.at/cadical;
